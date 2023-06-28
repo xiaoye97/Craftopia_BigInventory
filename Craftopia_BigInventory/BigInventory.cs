7b175e135a8f0e1baa4cdbcb6dc8d9e2c4adf4c3
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2023-06-28 20:22:19 +0800

    添加项目文件。

---
+++ Craftopia_BigInventory/BigInventory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Oc;
+using Oc.Item.UI;
+
+namespace BigInventory
+{
+    [BepInPlugin("com.github.xiaoye97.plugin.Craftopia.BigInventory", "BigInventory", "1.4")]
+    public class BigInventory : BaseUnityPlugin
+    {
+        public static int InventorySize = 100;
+        public static ManualLogSource logger;
+
+        public void Awake()
+        {
+            BigInventory.logger = base.Logger;
+            base.Logger.LogMessage("大背包Mod启动");
+            Harmony.CreateAndPatchAll(typeof(BigInventory), null);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(OcItemUI_Cell_List), "InitOnAwake", new Type[] { typeof(int) })]
+        public static bool GamePatch(OcItemUI_Cell_List __instance, ref int size)
+        {
+            OcItemUI_InventoryMng instUISS = UISceneSingleton<OcItemUI_InventoryMng>.InstUISS;
+            if ((int)__instance.itemType < instUISS.advancedExtendInventory.Length)
+            {
+                BigInventory.logger.LogMessage($"将{__instance.name} {__instance.itemType}的初始化尺寸从{size}设为{BigInventory.InventorySize}");
+                size = BigInventory.InventorySize;
+            }
+            return true;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(OcItemUI_Cell_List), "InitOnAwake", new Type[] { })]
+        public static bool GamePatch2(OcItemUI_Cell_List __instance)
+        {
+            if (!__instance.list.IsNullOrEmpty<OcItemUI_Cell>())
+            {
+                if (__instance.list.Count < __instance.Size)
+                {
+                    BigInventory.logger.LogMessage($"{__instance.name} {__instance.itemType}的list.Count {__instance.list.Count} < Size {__instance.Size}，进行补充");
+                    for (int i = __instance.list.Count; i < __instance.Size; i++)
+                    {
+                        __instance.list.Add(__instance.InstCell(i));
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
